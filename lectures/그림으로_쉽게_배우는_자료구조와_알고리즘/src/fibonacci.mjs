b432a12a1c3235bb15877a436b357fab49ea4767
HoonDongKang <d159123@naver.com>
2025-03-21 14:27:36 +0900

    feat: Add tabulation

--- lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/fibonacci.mjs
+++ lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/fibonacci.mjs
@@ -13,6 +13,17 @@ function fibonacci2(n, memo) {
     return memo[n];
 }
 
+function fibonacci3(n, memo) {
+    if (n == 0 || n == 1) return n;
+
+    let table = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        table[i] = table[i - 2] + table[i - 1];
+    }
+
+    return table[n];
+}
+
 console.time("first fibonacci");
 console.log(fibonacci(40));
 console.timeEnd("first fibonacci");
@@ -20,3 +31,7 @@ console.timeEnd("first fibonacci");
 console.time("second fibonacci");
 console.log(fibonacci2(40, {}));
 console.timeEnd("second fibonacci");
+
+console.time("third fibonacci");
+console.log(fibonacci3(40, {}));
+console.timeEnd("third fibonacci");
