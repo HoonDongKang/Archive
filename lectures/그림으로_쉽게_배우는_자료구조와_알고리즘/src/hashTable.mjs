1b6b99638be3fe11ad175085ddfef57a958e7942
Donghoon Kang <d159123@naver.com>
2025-03-23 23:56:28 +0900

    .

--- lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/hashTable.mjs
+++ lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/hashTable.mjs
@@ -19,12 +19,44 @@ class HashTable {
         return number % 10;
     }
 
+    hashNameFunction(name) {
+        // 1. 이름의 첫 자 유니코드로만 설정
+        // return name.charCodeAt(0) % 10;
+
+        // 2. 이름의 각 유니코드를 더한 값
+        // return [...name].reduce((acc, cur) => acc + cur.charCodeAt(0), 0) % 10;
+
+        // 3. 첫 자의 유니코드의 각 숫자를 합친 값
+        return [...(name.charCodeAt(0) + '')].reduce((acc, cur) => acc + cur.charCodeAt(0), 0) % 10;
+    }
+
+    getDuplicateFrequency(arr) {
+        let array = arr.map((name) => this.hashNameFunction(name));
+        const frequency = array.reduce((acc, cur) => {
+            acc[cur] = (acc[cur] || 0) + 1;
+            return acc;
+        }, {});
+
+        let duplicatedCount = 0;
+        let totalCount = array.length;
+
+        for (let key in frequency) {
+            if (frequency[key] > 1) {
+                duplicatedCount += frequency[key];
+            }
+        }
+
+        console.log(Math.round((duplicatedCount / totalCount) * 100 * 10) / 10);
+    }
+
     set(key, value) {
-        this.arr[this.hashFunction(key)].insertAt(0, new HashData(key, value));
+        let insertValue = value ? value : key;
+        console.log(this.hashNameFunction(key))
+        this.arr[this.hashNameFunction(key)].insertAt(0, new HashData(key, insertValue));
     }
 
     get(key) {
-        let currentNode = this.arr[this.hashFunction(key)].head;
+        let currentNode = this.arr[this.hashNameFunction(key)].head;
 
         while (currentNode != null) {
             if (currentNode.data.key === key) {
--- lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/test.hashTable.mjs
+++ lectures/그림으로_쉽게_배우는_자료구조와_알고리즘/src/test.hashTable.mjs
@@ -1,6 +1,6 @@
 import { HashTable, HashTableWithLinearProbing } from "./hashTable.mjs";
 
-// let hashTable = new HashTable();
+let hashTable = new HashTable();
 
 // hashTable.set(1, "이운재");
 // hashTable.set(4, "최진철");
@@ -20,24 +20,44 @@ import { HashTable, HashTableWithLinearProbing } from "./hashTable.mjs";
 
 // console.log(`1: ${hashTable.get(21)}`);
 
-const hashTable_2 = new HashTableWithLinearProbing();
+hashTable.set("이운재");
+hashTable.set("최진철");
+hashTable.set("홍명보");
+hashTable.set("유상철");
+hashTable.set("송중국");
+hashTable.set("박지성");
+hashTable.set("김남일");
+hashTable.set("이영표");
+hashTable.set("최태욱");
+hashTable.set("설기현");
+hashTable.set("이천수");
 
-hashTable_2.set(1, "이운재");
-hashTable_2.set(4, "최진철");
-hashTable_2.set(20, "홍명보");
-hashTable_2.set(6, "유상철");
-hashTable_2.set(22, "송중국");
-hashTable_2.set(21, "박지성");
-hashTable_2.set(5, "이김남일");
-hashTable_2.set(10, "이영표");
-hashTable_2.set(8, "최태욱");
-hashTable_2.set(9, "설기현");
-hashTable_2.set(14, "이천수");
+console.log(hashTable)
 
-console.log(`1: ${hashTable_2.get(21)}`);
-console.log(hashTable_2);
-hashTable_2.remove(21);
-console.log(hashTable_2);
-hashTable_2.set(21, "박지성");
-console.log(hashTable_2);
-console.log(`1: ${hashTable_2.get(1)}`);
+console.log(`이운재: ${hashTable.get('이운재')}`);
+
+hashTable.remove('이운재');
+
+console.log(`이운재: ${hashTable.get('이운재')}`);
+
+// const hashTable_2 = new HashTableWithLinearProbing();
+
+// hashTable_2.set(1, "이운재");
+// hashTable_2.set(4, "최진철");
+// hashTable_2.set(20, "홍명보");
+// hashTable_2.set(6, "유상철");
+// hashTable_2.set(22, "송중국");
+// hashTable_2.set(21, "박지성");
+// hashTable_2.set(5, "이김남일");
+// hashTable_2.set(10, "이영표");
+// hashTable_2.set(8, "최태욱");
+// hashTable_2.set(9, "설기현");
+// hashTable_2.set(14, "이천수");
+
+// console.log(`1: ${hashTable_2.get(21)}`);
+// console.log(hashTable_2);
+// hashTable_2.remove(21);
+// console.log(hashTable_2);
+// hashTable_2.set(21, "박지성");
+// console.log(hashTable_2);
+// console.log(`1: ${hashTable_2.get(1)}`);
